9b252785ebefaffd280b564f2fa36e20c395983e
Karwan Ali <karwana0000@gmail.com>
2025-11-18 10:22:03 +0100

    writing client

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
+
+    int port = 333333;
+    public Client() throws IOException {
+
+        Socket socket = new Socket("127.0.0.1", 333333);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        String temp;
+
+        while ((temp = in.readLine()) != null) {
+            out.println(temp);
+        }
+
+    }
 }
